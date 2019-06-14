8ceb7a6ca48c722908c28a7581cbedee52725f97
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-06-14 13:28:40 -0400

    basic ui to try and push parks into

---
+++ assets/js/parks.js
@@ -0,0 +1,48 @@
+    // Array for parks 
+    var nationalParks = [
+        
+    ]; 
+
+    // Var for Zip Code 
+    var zipcode = ""; 
+
+    // Vars for query URL 
+    // api key
+    var apiKey = ""
+    //query URL 
+    var parkQueryURL = "http://developer.nps.gov/api/v1/parks?stateCode=PA&limit=10&api_key=TUzbrDxmdtDfjNLGofmsOXAmQ6WPMwukeORXBBHm"; 
+    
+
+    parkFetcher(); 
+
+    function parkFetcher () { 
+        console.log("parkFetcher");
+
+        $.ajax ({
+            url:parkQueryURL, 
+            method: "GET"
+        })
+
+        .then(function (response){
+            var parks = response.data; 
+            console.log(parks); 
+
+            for (var i = 0; i < parks.length; i++) { 
+                var currentPark =  parks[i]
+                var splitValue = currentPark.latLong.split(", ");
+                var lat = splitValue[0].split(":")[1]; 
+                var long = splitValue[1].split(":")[1];  
+                nationalParks.push({
+                    name:currentPark.fullName,
+                    type:currentPark.designation, 
+                    lat, 
+                    long, 
+                    url:currentPark.url, 
+                }); 
+                console.log(nationalParks); 
+            }
+
+        })   
+    }
+
+    
